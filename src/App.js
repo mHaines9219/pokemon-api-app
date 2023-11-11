e28b7c8cb6e7493329b249e3950f8efb02536d5e
mHaines331 <mhaines9219@gmail.com>
2023-11-11 11:50:51 -0800

    add axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useState } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Pokemon</div>;
+  const [pokemon, setPokemon] = useState([]);
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    setPokemon(res.data.results.map((p) => p.name));
+  });
+  return <PokemonList pokemon={pokemon} />;
 }
 
 export default App;
